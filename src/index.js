4a214c213599239ba0bdfba2e88c3b4a77ee2417
lealalves <leealalves@hotmail.com>
2022-07-21 11:21:39 -0300

    implementado sistema login/registro

--- src/index.js
+++ src/index.js
@@ -2,23 +2,128 @@ import path from 'path'
 import http from 'http'
 import Express from 'express'
 import socketio from 'socket.io'
+import mongoose from 'mongoose'
+import User from '../src/models/User'
+import livereload from 'livereload'
+import connectLiveReload from 'connect-livereload'
+import session from 'express-session'
+import 'dotenv/config'
+
+const liveReloadServer = livereload.createServer();
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+})
 
 const app = Express()
 const server = http.createServer(app)
 const io = socketio(server)
 const port = process.env.PORT || 3000
 
-app.use(Express.static(path.join(__dirname, 'public')))
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apicluster.kbp4k.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => console.log('Banco conectado!'))
+.catch((err) => console.log('Erro ao conectar ao banco' + err))
 
-server.listen(port, () => {
-	console.log('Server listening at port %d', port)
-})
+app.use(session({
+	secret: 'cat',
+	resave: false,
+	saveUninitialized: false,
+}))
+app.use(connectLiveReload())
+app.use(Express.static(path.join(__dirname, 'public')))
 
 let onlineUsers = 0
 
 io.on('connection', socket => {
 	let addedUser = false
-	
+
+  socket.on('loginUser', data => {
+    const {email, password} = data
+
+    let errors = []
+
+    if(!email){
+      errors.push('Usuário inválido')
+    }
+    if(!password){
+      errors.push('Senha inválido')
+    }
+
+    if(errors.length > 1){
+      return console.log(errors)
+    } else{
+      User.findOne({email: email, password: password})
+      .then((user) => {
+        if(!user){
+          return console.log('Senha/Usuário inválidos, por favor digite novamente.')
+        } else {
+          console.log('Logado com sucesso!')
+          if (addedUser) return
+		
+          socket.username = user.name
+          ++onlineUsers
+          addedUser = true
+          
+          socket.emit('validUser', user.name)
+
+          socket.emit('login', {
+            onlineUsers
+          })
+          
+          socket.broadcast.emit('userJoined', {
+            username: user.name,
+            onlineUsers
+          })
+          
+          console.log(`User ${user.name} added`)
+        }
+      })
+      .catch((err) => console.log('Houve um erro interno' + err))
+    }
+  })
+
+  socket.on('newUser', data => {    
+    const {username, email, password} = data
+
+    let errors = []
+
+    if(!username){
+      errors.push('Usuário inválido')
+    }
+    if(!email){
+      errors.push('E-mail inválido')
+    }
+    if(!password){
+      errors.push('Senha inválido')
+    }
+
+    if(errors.length > 1){
+      return console.log(errors);
+    } else {
+      User.findOne({email: email})
+      .then((user) => {
+        if(user){
+          console.log('Já existe um usuário com este e-mail')
+          return
+        }
+        const newUser = new User ({
+          name: username,
+          email: email,
+          password: password
+        })
+        newUser.save()
+        .then(() => {
+          console.log('Registado com sucesso!')
+          socket.emit('sucessRegister')
+        })
+        .catch((err) => console.log('Erro ao registrar o usuário' + err))
+      })
+      .catch((err) => console.log('Houve um erro interno' + err))
+    }
+    
+  })
+
 	socket.on('newMessage', data => {
 		socket.broadcast.emit('newMessage', {
 			username: socket.username,
@@ -26,25 +131,6 @@ io.on('connection', socket => {
 		});
 		
 		console.log('New message', data)
-	})
-	
-	socket.on('addUser', (username) => {
-		if (addedUser) return
-		
-		socket.username = username
-		++onlineUsers
-		addedUser = true
-		
-		socket.emit('login', {
-			onlineUsers
-		})
-		
-		socket.broadcast.emit('userJoined', {
-			username: socket.username,
-			onlineUsers
-		})
-		
-		console.log(`User ${username} added`)
 	})
 	
 	socket.on('typing', () => {
@@ -69,4 +155,8 @@ io.on('connection', socket => {
 			})
 		}
 	})
+})
+
+server.listen(port, () => {
+	console.log('Server listening at port %d', port)
 })
