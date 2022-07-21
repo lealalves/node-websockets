d2d1f3053b48aa90f312696d5f1fa863a756ecef
lealalves <leealalves@hotmail.com>
2022-07-21 11:15:20 -0300

    create User model file with mongoose

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+export default User
